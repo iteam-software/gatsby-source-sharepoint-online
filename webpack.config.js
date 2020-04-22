134439ed38f51cccc15bbdc17fcb14a361f973ad
Matt Lynam <matt@slash.gg>
2020-04-22 15:53:32 -0600

    feat: convert to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,52 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const pluginPath = "plugins/gatsby-source-sharepoint-online";
+const outputPath = path.resolve(
+  __dirname,
+  "plugins/gatsby-source-sharepoint-online"
+);
 
 module.exports = {
+  mode: "production",
   target: "node",
-  entry: "./src",
+  entry: "./src/gatsby-node.js",
   output: {
     filename: "gatsby-node.js",
-    path: path.resolve(__dirname, pluginPath),
+    path: outputPath,
+    libraryTarget: "commonjs2",
   },
-  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["@babel/plugin-proposal-class-properties"],
+            presets: [["@babel/preset-env", { targets: { esmodules: true } }]],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {
+        from: "LICENSE",
+        to: outputPath,
+      },
+      {
+        from: "package.json",
+        to: outputPath,
+        transform(content, path) {
+          const pkg = JSON.parse(content.toString("utf-8"));
+          delete pkg.devDependencies;
+          return new Buffer(JSON.stringify(pkg, null, 2), "utf-8");
+        },
+      },
+    ]),
+  ],
 };
