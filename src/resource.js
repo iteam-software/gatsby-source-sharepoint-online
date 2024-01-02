fdc78b31dd62be690104ce1939feb773caf1cf7a
Christopher Salinas <csalinas@iteamnm.com>
2024-01-02 14:02:31 -0700

    fix(resource): remove _

--- src/resource.js
+++ src/resource.js
@@ -64,7 +64,7 @@ class Resource {
       entry.value.forEach((data) => {
         helpers.actions.createNode({
           data,
-          id: helpers.createNodeId(`${normalizedListName}_${data.id}`),
+          id: helpers.createNodeId(`${normalizedListName}${data.id}`),
           parent: null,
           children: [],
           internal: {
