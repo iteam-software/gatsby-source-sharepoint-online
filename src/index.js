7f1b4598f8dc8d6507af296f00f35a7092d65830
Christopher Salinas <csalinas@iteamnm.com>
2024-01-02 23:50:41 -0700

    refactor: debug logging

--- src/index.js
+++ src/index.js
@@ -11,31 +11,38 @@ import Resource from "./resource.js";
  * @param {any} config Config object provided in the plugin config.
  */
 export const sourceNodes = async (helpers, config) => {
+  const { reporter } = helpers;
   const { host, sites = [], ...creds } = config;
   const client = createClient(creds);
   const listResource = new Resource("list");
+
+  reporter.info(`Generating nodes from SharePoint.`);
 
   for (let i = 0; i < sites.length; i++) {
     const { lists = [] } = sites[i];
     const get = listResource.requestFactory(host, sites[i], client, helpers);
 
-    helpers.reporter.info(`Working on site: ${sites[i].name}`);
+    reporter.info(`Working on site: ${sites[i].name}`);
 
     for (let j = 0; j < lists.length; j++) {
       const list = lists[j];
 
-      helpers.reporter.info(`Sourcing nodes for list: ${list[i].title}`);
+      reporter.info(`Sourcing nodes for list: ${list[i].title}`);
 
       if (!listResource.validate(list)) {
-        helpers.reporter.error(`${list[i].title} is not a valid list.`);
+        reporter.error(`${list[i].title} is not a valid list.`);
         continue;
       }
 
       try {
         await get(list);
       } catch (err) {
-        helpers.reporter.error(err);
+        reporter.panicOnBuild(err);
       }
     }
   }
+};
+
+export const onPluginInit = ({ reporter }) => {
+  reporter.info(`Hello World!`);
 };
